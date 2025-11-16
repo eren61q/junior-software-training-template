1ad477295802c4d763abc026ddea033ee120f721
eren61q <erenseyhan2161@gmail.com>
2025-11-16 22:14:17 +0300

    Intake constructor added

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,7 +1,5 @@
-public class IntakeSubsystem {
-    
 
-import com.revrobotics.RelativeEncoder;
+mport com.revrobotics.RelativeEncoder;
 import com.revrobotics.sim.SparkRelativeEncoderSim;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -20,33 +18,32 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
-import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;i
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import main.java.frc.robot.Constants;
 import main.java.frc.robot.subsystems.ElevatorSubsystem.ControlMode;
 
 import java.util.function.DoubleSupplier;
+@Logged(name = "Intake")
+public class IntakeSubsystem {
 
-Logged(@name = "Intake")
+    private final SparkMax intakeMotor = new SparkMax(Constants.Intake.intakeMotor, MotorType.kBrushless);
+    private final DigitalInput sensor = new DigitalInput(Constants.Intake.sensorPort);
 
-SparkMax motor = new SparkMax(canID, MotorType.kBrushless);
-
-public class IntakeSubsystem{
-    SparkMax intakeMotor = new SparkMax(Constants.Intake.intakeMotor, MotorType.kBrushless);
-    
     public double getVoltage() {
         return intakeMotor.getAppliedOutput() * intakeMotor.getBusVoltage();
-}
-  
-    public void setVoltage(double voltage) {
-        intakeMotor.setVoltage(voltage);
-
-    public void stop()
-        intakeMotor.setVoltage(0);
-
-    public boolean hasCoral(){
-        return sensor.get();
     }
 
-}}
+    public void setVoltage(double voltage) {
+        intakeMotor.setVoltage(voltage);
+    }
+
+    public void stop() {
+        intakeMotor.setVoltage(0);
+    }
+
+    public boolean hasCoral() {
+        return sensor.get();
+    }
+}
