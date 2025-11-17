c4c5e353cef402ce09ed5b289de107fded27dcf4
eren61q <erenseyhan2161@gmail.com>
2025-11-17 21:05:28 +0300

    climb fix-- slave removed

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,4 +1,6 @@
-import main.java.frc.robot.Constants;
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.sim.SparkRelativeEncoderSim;
 import com.revrobotics.spark.SparkBase.PersistMode;
@@ -21,31 +23,27 @@ import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import main.java.frc.robot.Constants;
-import main.java.frc.robot.subsystems.ElevatorSubsystem.ControlMode;
 
 import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
 @Logged(name = "Intake")
 public class ClimbSubsystem {
 
-    private final SparkMax masterMotor = new SparkMax(Constants.Climb.masterMotor, MotorType.kBrushless);
-    private final SparkMax slaveMotor = new SparkMax(Constants.Climb.slaveMotor, MotorType.kBrushless);
+    private final SparkMax masterMotor;
 
-    private final RelativeEncoder masterEncoder = masterMotor.getEncoder();
-    private final RelativeEncoder slaveEncoder = slaveMotor.getEncoder();
-
-    private final PIDController pidController = new PIDController();
+    private final RelativeEncoder masterEncoder;
 
     private double targetMasterPosition;
-    private double targetSlavePosition;
 
-    private double ffMaster = 1; //determine
-    private double ffSlave = 1; //determine
+    private double ffMaster = 1; // determine
 
     private ClimbState state = ClimbState.IDLE;
+
+    public ClimbSubsystem() {
+        masterMotor = new SparkMax(Constants.Climb.Climber.motorID, MotorType.kBrushless);
+        masterEncoder = masterMotor.getEncoder();
+    }
 
     public enum ClimbState {
         IDLE,
@@ -55,11 +53,7 @@ public class ClimbSubsystem {
     }
 
     public double getMasterAngle() {
-        return masterEncoder.getPosition() * 360.0; //position in angle
-    }
-
-    public double getSlaveAngle() {
-        return slaveEncoder.getPosition() * 360.0;  //position in angle
+        return masterEncoder.getPosition() * 360.0; // position in angle
     }
 
     public void setState(ClimbState newState) {
@@ -68,17 +62,17 @@ public class ClimbSubsystem {
         switch (state) {
             case HOLDING:
                 targetMasterPosition = masterEncoder.getPosition();
-                targetSlavePosition = slaveEncoder.getPosition();
+
                 break;
 
             case OPENING:
                 targetMasterPosition += 1.0; // adjust
-                targetSlavePosition += 1.0;
+
                 break;
 
             case CLOSING:
                 targetMasterPosition -= 1.0;
-                targetSlavePosition -= 1.0;
+
                 break;
 
             case IDLE:
@@ -86,36 +80,38 @@ public class ClimbSubsystem {
                 // do nothing
                 break;
         }
-  }
+    }
 
-    //should be in periodic(check later)
+    // should be in periodic(check later)
     public void update() {
         switch (state) {
             case OPENING:
-                double masterVoltage = pidController.calculate(getMasterAngle(), targetMasterPosition * 360.0);
-                double slaveVoltage = pidController.calculate(getSlaveAngle(), targetSlavePosition * 360.0);
 
-                masterMotor.setVoltage(masterVoltage);
-                slaveMotor.setVoltage(slaveVoltage);
+                masterMotor.setVoltage(0);
+
                 break;
             case CLOSING:
-                double masterVoltage = pidController.calculate(getMasterAngle(), targetMasterPosition * 360.0);
-                double slaveVoltage = pidController.calculate(getSlaveAngle(), targetSlavePosition * 360.0);
 
-                masterMotor.setVoltage(masterVoltage);
-                slaveMotor.setVoltage(slaveVoltage);
+                masterMotor.setVoltage(0);
+
                 break;
             case HOLDING:
 
                 masterMotor.setVoltage(ffMaster);
-                slaveMotor.setVoltage(ffSlave);
                 break;
 
             case IDLE:
             default:
                 masterMotor.setVoltage(0);
-                slaveMotor.setVoltage(0);
                 break;
+        }
+
     }
-}   
+
+    public SparkMaxConfig SparkMaxConfig() {
+        SparkMaxConfig motorConfig = new SparkMaxConfig();
+        motorConfig.idleMode(IdleMode.kBrake).voltageCompensation(12).smartCurrentLimit(40);
+        return motorConfig;
+    }
+
 }
