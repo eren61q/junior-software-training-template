e9230951c5a2170b997fedef77fd0e188b602793
eren61q <erenseyhan2161@gmail.com>
2025-11-17 22:35:56 +0300

    elevator methods added 🛌

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,11 +1,47 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.AlternateEncoderConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
+  SparkMax masterMotor;
+  RelativeEncoder elEncoder;
+  AlternateEncoderConfig alternateEncoderConfig;
+
   public ElevatorSubsystem() {
+    masterMotor = new SparkMax(Constants.Elevator.masterID, MotorType.kBrushless);
+    elEncoder = masterMotor.getAlternateEncoder();
+    alternateEncoderConfig = new AlternateEncoderConfig();
     
+  }
+
+  public SparkMaxConfig SparkMaxConfig() {
+    SparkMaxConfig motorConfig = new SparkMaxConfig();
+    motorConfig.idleMode(IdleMode.kBrake).voltageCompensation(12).smartCurrentLimit(40);
+    return motorConfig;
+  }
+
+  public void setPosition() {
+
+  }
+
+  public void stop() {
+    masterMotor.set(0);
+  }
+
+  public double getPosition() {
+    return elEncoder.getPosition();
+  }
+
+  public void holdElevatorAtPosition() {
+
   }
 
   @Override
