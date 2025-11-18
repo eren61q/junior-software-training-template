09c909d2675886d4a05c75da797c08f8ec128668
eren61q <erenseyhan2161@gmail.com>
2025-11-18 22:49:11 +0300

    Elevator commands added + fix

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,25 +1,35 @@
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.AlternateEncoderConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   SparkMax masterMotor;
   RelativeEncoder elEncoder;
   AlternateEncoderConfig alternateEncoderConfig;
+  PIDController pidController;
+  private double targetPosition;
+  ElevatorFeedforward ff = new ElevatorFeedforward(Constants.Elevator.kS, Constants.Elevator.kG, Constants.Elevator.kV);
+  double voltage = ff.calculate(masterMotor.get());
 
   public ElevatorSubsystem() {
     masterMotor = new SparkMax(Constants.Elevator.masterID, MotorType.kBrushless);
     elEncoder = masterMotor.getAlternateEncoder();
     alternateEncoderConfig = new AlternateEncoderConfig();
-    
+    pidController = new PIDController(2.2, 0, 0);
+
   }
 
   public SparkMaxConfig SparkMaxConfig() {
@@ -28,8 +38,13 @@ public class ElevatorSubsystem extends SubsystemBase {
     return motorConfig;
   }
 
-  public void setPosition() {
+  public void setVoltage(double voltage) {
+    masterMotor.setVoltage(voltage);
+  }
 
+  public void setPosition(double newTargetMeters) {
+    targetPosition = newTargetMeters;
+    masterMotor.setVoltage(pidController.calculate(getPosition(), targetPosition));
   }
 
   public void stop() {
@@ -41,12 +56,42 @@ public class ElevatorSubsystem extends SubsystemBase {
   }
 
   public void holdElevatorAtPosition() {
+    masterMotor.setVoltage(voltage);
 
+  }
+
+  public Command holdEl() {
+    return runOnce(() -> holdElevatorAtPosition());
+
+  }
+
+  public Command stopEl() {
+    return runOnce(() -> stop());
+
+  }
+
+  public Command l1Score() {
+    return runOnce(() -> setPosition(Constants.Climb.Levels.L1_SCORE));
+  }
+
+  public Command l2Score() {
+
+    return runOnce(() -> setPosition(Constants.Climb.Levels.L2));
+  }
+
+  public Command l3Score() {
+    return runOnce(() -> setPosition(Constants.Climb.Levels.L3));
+  }
+
+  public Command l4Score() {
+    return runOnce(() -> setPosition(Constants.Climb.Levels.L4_SCORE));
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Current Position", getPosition());
+    SmartDashboard.putNumber("Current Voltage", masterMotor.getAppliedOutput());
+
   }
 
   @Override
